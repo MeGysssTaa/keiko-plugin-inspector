30c38c561882f6b80def248fdece82268dc40148
DarksideCode <darksidecode@ya.ru>
2020-06-19 19:20:54 +0200

    Improve regex handling in DAC rules' arguments

--- src/main/java/me/darksidecode/keiko/util/StringUtils.java
+++ src/main/java/me/darksidecode/keiko/util/StringUtils.java
@@ -16,19 +16,31 @@
 
 package me.darksidecode.keiko.util;
 
+import java.util.regex.Pattern;
+
 public final class StringUtils {
 
     private StringUtils() {}
 
     private static final String NO_WILDCARDS_PREFIX = "NO_WILDCARDS :: ";
+    private static final String ESCAPED_DOT         = Pattern.quote(".");
 
     public static boolean matchWildcards(String text, String pattern) {
         if (pattern.startsWith(NO_WILDCARDS_PREFIX))
             return text.equals(pattern.replace(NO_WILDCARDS_PREFIX, ""));
 
-        return text.matches(pattern.
-                replace("?", ".?").
-                replace("*", ".*?"));
+        return text.matches(pattern
+                // Allow ordinary dots "." in rules' arguments without forcing the user
+                // to escape it manually, because ordinary dots "." are very often used
+                // in rules' arguments.
+                .replace(".", ESCAPED_DOT)
+                // Regex (simplified, i.e. dot-reduced). These characters are not
+                // as widely used as dots ".", so users have to escape them manually
+                // if they need to.
+                .replace("*", ".*")
+                .replace("+", ".+")
+                .replace("?", ".?")
+        );
     }
 
     public static String replacePortByName(String s) {
