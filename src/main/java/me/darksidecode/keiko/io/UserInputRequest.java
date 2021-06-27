05e1ddc9aabdea1eb0ffb26e036765f4be36be71
DarksideCode <darksidecode@ya.ru>
2021-06-27 09:44:55 +0200

    Use UTF-8 in UserInputRequest#tryRead

--- src/main/java/me/darksidecode/keiko/io/UserInputRequest.java
+++ src/main/java/me/darksidecode/keiko/io/UserInputRequest.java
@@ -24,6 +24,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -85,7 +86,8 @@ public class UserInputRequest<T> {
     private synchronized T tryRead() {
         prompter.prompt(prompt);
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             String line = reader.readLine();
 
             if (line == null)
