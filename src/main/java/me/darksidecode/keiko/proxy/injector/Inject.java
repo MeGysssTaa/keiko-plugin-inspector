c1f243b2777382966a8ab2ffdbbd4ff6f91e9d31
DarksideCode <darksidecode@ya.ru>
2021-06-30 12:06:30 +0200

    Add some comments (pseudo-doc) in @Inject

--- src/main/java/me/darksidecode/keiko/proxy/injector/Inject.java
+++ src/main/java/me/darksidecode/keiko/proxy/injector/Inject.java
@@ -24,14 +24,40 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * If put on a type that inherits from abstract class Injection, indicates that this class's
+ * overriden apply(...) method should be called on a ClassNode+MethodNode pair that satisfies
+ * the requirements provided with annotation parameters. Enables creation of arbitrary class
+ * bytecode transformers that can remove or add any bytecode in any place.
+ *
+ * If put on a public static method without parameters, indicates that a call to this method
+ * should be inserted at the given position of a ClassNode+MethodNode pair that satisfies the
+ * requirements provided with annotation parameters. Makes it very easy to append arbitrary
+ * Java code at a certain position of a method, without having to hussle with bytecode/ASM.
+ */
 @Target ({ ElementType.TYPE, ElementType.METHOD })
 @Retention (RetentionPolicy.RUNTIME)
 public @interface Inject {
 
+    /**
+     * Fully qualified name of the class where the injection should be applied.
+     * Package separator must be either '.' or '/' (both will work)..
+     */
     String inClass();
 
+    /**
+     * Name and descriptor of the method of the class 'inClass()' where the injection should be applied,
+     * without any separators. For example, if a method is defined as "public static int foo()", then the
+     * 'inMethod()' parameter should look like "foo()I" (see JVM method descriptor specification for details).
+     */
     String inMethod();
 
+    /**
+     * Relative insertion position (only used for MethodCallInjection).
+     * Cannot be UNUSED for MethodCallInjection, but may be anything for other Injections.
+     *
+     * @see me.darksidecode.keiko.proxy.injector.injection.MethodCallInjection
+     */
     Position at() default Position.UNUSED;
 
     /**
