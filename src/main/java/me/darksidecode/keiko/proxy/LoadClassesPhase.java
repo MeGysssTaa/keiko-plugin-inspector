b86146cd112aeec18486d0a4d9385967050a841d
DarksideCode <darksidecode@ya.ru>
2021-06-30 12:56:37 +0200

    Add NMS (Minecraft Server) version detection in LoadClassesPhase

--- src/main/java/me/darksidecode/keiko/proxy/LoadClassesPhase.java
+++ src/main/java/me/darksidecode/keiko/proxy/LoadClassesPhase.java
@@ -72,12 +72,20 @@ class LoadClassesPhase extends Phase<JarFile, LoadClassesPhase.Result> {
 
             if (entry.getName().endsWith(".class")) {
                 try {
-                    classLoader.findClass(entry.getName()
+                    String className = entry.getName()
                             .replace('/', '.')
-                            .replace(".class", "")
-                    );
+                            .replace(".class", "");
 
+                    classLoader.findClass(className);
                     result.successes++;
+
+                    // Detect Minecraft server version from package name.
+                    if (Keiko.INSTANCE.getEnv().getNmsVersion() == null
+                            && className.startsWith("net.minecraft.server.")) {
+                        String nmsVersion = className.split("\\.")[3]; // infer from package name
+                        Keiko.INSTANCE.getLogger().debug("Minecraft server: %s", nmsVersion);
+                        Keiko.INSTANCE.getEnv().setNmsVersion(nmsVersion);
+                    }
                 } catch (Throwable t) {
                     if (JMinima.debug) t.printStackTrace();
                     errMsgBuilder.append("\n    - ").append(t);
