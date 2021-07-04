211b0869062175ce2927ad8fcf8997943df686d0
DarksideCode <darksidecode@ya.ru>
2021-07-04 12:55:17 +0200

    Fix ForceOp remediation not working (make events async)

--- src/main/java/me/darksidecode/keiko/runtimeprotect/megane/event/EventBus.java
+++ src/main/java/me/darksidecode/keiko/runtimeprotect/megane/event/EventBus.java
@@ -26,25 +26,45 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class EventBus {
 
     private final Map<Class<? extends Event>, Collection<Listener>> listeners = new HashMap<>();
+
+    /**
+     * Dispatch all events in a separate thread. The rationale behind this is:
+     *
+     *     1. We want our checks to run in background, without interrupting or pausing any
+     *        actions. We don't want the server to lag if one of our heuristics lags.
+     *
+     *     2. This allows us to react to events seamlessly from another thread. For example,
+     *        we cannot de-op a player right inside the OpUpdate event listener because that
+     *        task involves writing to a file, and since the event is called before the initial
+     *        (original) write to the ops file, our de-opping file write will be overwritten, or,
+     *        in other words, will not work. But from another thread, we can easily do so.
+     *
+     * We don't really want multiple threads to be used for event dispatch, though, or at least
+     * that's my thoughts at the moment. Events are better synchronized and ordered as they were.
+     */
+    private final Executor executor = Executors.newSingleThreadExecutor();
 
     public Collection<Listener> getListenersOf(Class<? extends Event> eventType) {
         return listeners.computeIfAbsent(eventType, k -> new ArrayList<>());
     }
 
     public void dispatchEvent(@NonNull Event event) {
-        Collection<Listener> listeners = getListenersOf(event.getClass());
+        executor.execute(() -> {
+            Collection<Listener> listeners = getListenersOf(event.getClass());
 
-        for (Listener listener : listeners) {
-            try {
-                event.dispatch(listener);
-            } catch (Exception ex) {
-                Keiko.INSTANCE.getLogger().error("Unhandled exception in a Megane event listener", ex);
+            for (Listener listener : listeners) {
+                try {
+                    event.dispatch(listener);
+                } catch (Exception ex) {
+                    Keiko.INSTANCE.getLogger().error("Unhandled exception in a Megane event listener", ex);
+                }
             }
-        }
+        });
     }
 
 }
